8b1f86aa9884e4847184f797b2d4d2c245be9684
Bima595 <satriaabimanyu595@gmail.com>
2023-10-10 14:12:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function input(){
+    const satu = document.getElementById('1');
+    const dua = document.getElementById('2');
+    const tiga = document.getElementById('3');
+    const empat = document.getElementById('4');
+    const lima = document.getElementById('5');
+    const enam = document.getElementById('6');
+    const tujuh = document.getElementById('7');
+    const delapan = document.getElementById('8');
+    const sembilan = document.getElementById('9');
+    const enol = document.getElementById('0');
+    const double = document.getElementById('00');
+
+}
+
+function hasil(){
+
+    if(tambah){
+
+    }
+}
+
+function hasil(){
+
+}
